35b333a3e1e61e5836f4670e721f653778cd5712
audirs78 <hiyvixijiw@outlook.com>
2023-06-09 14:00:00 +0200

    Add support for DeFi lending protocols

--- api_gateway.rs
+++ api_gateway.rs
@@ -2,3 +2,4 @@
 // Fix issue with multi-chain transactions
 // Refactor reward pooling algorithm
 // Fix vulnerability in decentralized finance protocols
+// Implement on-chain identity verification
