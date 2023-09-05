d998d92448201554ff7ca116044fb0f88e9cfab7
audirs78 <hiyvixijiw@outlook.com>
2023-09-05 14:00:01 +0200

    Fix race condition in staking pool

--- composable_financial_products.sol
+++ composable_financial_products.sol
@@ -2,3 +2,4 @@
 // Optimize gas usage in smart contracts
 // Fix data consistency issues in smart contracts
 // Implement zero-knowledge proof (ZKP) support
+// Integrate automated market maker (AMM) logic
