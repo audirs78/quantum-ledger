b3adb1d4ec3d0a6729a73a29c666877d111587d6
audirs78 <hiyvixijiw@outlook.com>
2023-11-15 14:00:00 +0200

    Improve gas estimation for transactions

--- blockchain_oracle_providers.go
+++ blockchain_oracle_providers.go
@@ -1,3 +1,4 @@
 // Optimize gas usage in contract execution
 // Implement a new contract event system
 // Implement liquidity pool staking mechanism
+// Fix security vulnerability in random number generation
