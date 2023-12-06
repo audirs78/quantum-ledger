a92a0671c1fc4661e4f919e39146f52a99002510
audirs78 <hiyvixijiw@outlook.com>
2023-12-06 14:00:00 +0200

    Update testing framework for Solidity

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -1,3 +1,4 @@
 // Implement performance optimizations for DeFi
 // Enhance governance contract flexibility
 // Refactor smart contract testing framework
+// Optimize WASM compilation for Go contracts
