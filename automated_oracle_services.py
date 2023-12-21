935ffaff19b062e9d3741f07a35dc1b0f1c53400
audirs78 <hiyvixijiw@outlook.com>
2023-12-21 14:00:02 +0200

    Refactor governance token logic

--- automated_oracle_services.py
+++ automated_oracle_services.py
@@ -2,3 +2,4 @@
 // Add support for multi-signature wallet contracts
 // Integrate automated market maker (AMM) logic
 // Integrate staking rewards automation
+// Fix deserialization bug in Haskell code
