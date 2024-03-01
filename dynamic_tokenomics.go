a28751e58a25b2785195692005b95d79326d264f
audirs78 <hiyvixijiw@outlook.com>
2024-03-01 14:00:00 +0200

    Add dynamic contract fees

--- dynamic_tokenomics.go
+++ dynamic_tokenomics.go
@@ -1,3 +1,4 @@
 // Implement contract function batching
 // Fix issues with gas optimization in smart contracts
 // Implement decentralized arbitration in contracts
+// Optimize WASM compilation for Go contracts
