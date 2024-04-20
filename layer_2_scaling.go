c5e69b669bcf6545fc3aa8d2c63b136b4427265b
audirs78 <hiyvixijiw@outlook.com>
2024-04-20 14:00:01 +0200

    Fix ERC-721 contract issues with transfers

--- layer_2_scaling.go
+++ layer_2_scaling.go
@@ -1,3 +1,4 @@
 // Integrate with decentralized identity providers
 // Fix bugs in decentralized exchange routing
 // Integrate decentralized anonymous identity systems
+// Fix issue in ERC-20 token transfers
