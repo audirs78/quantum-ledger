e37b7b02c353146deded0aca8dbf4aa338c33b6d
audirs78 <hiyvixijiw@outlook.com>
2024-05-01 14:00:01 +0200

    Integrate IPFS for storage

--- decentralized_auction_system.go
+++ decentralized_auction_system.go
@@ -2,3 +2,4 @@
 // Fix issues with contract finalization
 // Refactor multi-signature wallet code
 // Add support for decentralized content distribution
+// Refactor storage layout for better efficiency
