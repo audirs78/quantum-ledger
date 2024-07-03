18cb4a89f55a93da424771645c6f9242ef1db1de
audirs78 <hiyvixijiw@outlook.com>
2024-07-03 14:00:00 +0200

    Refactor reward pooling algorithm

--- liquidity_provider_incentives.rs
+++ liquidity_provider_incentives.rs
@@ -3,3 +3,4 @@
 // Add native support for staking rewards
 // Improve security in decentralized applications
 // Fix interoperability issues with other blockchains
+// Implement rewards escrow system
