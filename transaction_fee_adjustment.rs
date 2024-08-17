e7fb34791c7d1d9c1e432c054046d051b674602a
audirs78 <hiyvixijiw@outlook.com>
2024-08-17 14:00:01 +0200

    Refactor validator reward system

--- transaction_fee_adjustment.rs
+++ transaction_fee_adjustment.rs
@@ -2,3 +2,4 @@
 // Optimize proof-of-stake validation
 // Add support for decentralized credit scoring
 // Add support for zk-SNARKs in smart contracts
+// Improve scalability for smart contract platforms
