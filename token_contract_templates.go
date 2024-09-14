0b5b1c14216e538d0196aae0428ed9b3e687b143
audirs78 <hiyvixijiw@outlook.com>
2024-09-14 14:00:00 +0200

    Add multi-sig wallet functionality

--- token_contract_templates.go
+++ token_contract_templates.go
@@ -4,3 +4,4 @@
 // Refactor decentralized exchange (DEX) code
 // Refactor token distribution mechanism
 // Add support for decentralized file storage
+// Add support for zk-SNARKs in smart contracts
